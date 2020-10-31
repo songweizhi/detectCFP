961b793b66afc60460e1c8e4f859c41f3e292fc1
songweizhi <songwz03@gmail.com>
2020-10-31 14:56:29 +1100

    -

---
+++ README.md
@@ -0,0 +1,25 @@
+
+## detectCFP (Detect carbon fixation pathways)
+
+[![pypi licence](https://img.shields.io/pypi/l/detectCFP.svg)](https://opensource.org/licenses/gpl-3.0.html)
+[![pypi version](https://img.shields.io/pypi/v/detectCFP.svg)](https://pypi.python.org/pypi/detectCFP) 
+
+
+Dependencies
+---
+
+[gapseq](https://github.com/jotech/gapseq)
+
+
+How to install:
+---
+
+detectCFP can be installed via `pip3`:
+
+    # First-time installation
+    pip3 install detectCFP
+        
+    # upgrade
+    pip3 install --upgrade detectCFP
+
+
---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+
+
+def version():
+
+    setup_dir = os.path.dirname(os.path.realpath(__file__))
+    version_file = open(os.path.join(setup_dir, 'detectCFP', 'VERSION'))
+
+    return version_file.readline().strip()
+
+
+__long_description__ = '''
+
+detectCFP: Detect carbon fixation pathways
+Weizhi Song (songwz03@gmail.com)
+Center for Marine Science & Innovation (CMSI)
+University of New South Wales, Sydney, Australia
+
+'''
+
+setup(name="detectCFP",
+      version=version(),
+      long_description=__long_description__,
+      license="GPL3+",
+      author="Weizhi Song",
+      author_email="songwz03@gmail.com",
+      keywords="Bioinformatics CarbonFixationPathway",
+      description="Detect carbon fixation pathways",
+      url="https://github.com/songweizhi/detectCFP",
+      packages=['MarkerMAG'],
+      package_data={'': ['*.r', '*.R', '*.py', 'VERSION']},
+      include_package_data=True,
+      install_requires=['biopython', 'pandas'],
+      scripts=['bin/detectCFP'])
